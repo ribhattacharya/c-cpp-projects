d85935b31a3b02ebabd7fd3f7907a143e059810c
Rishabh Bhattacharya <ribhattacharya@ucsd.edu>
2023-07-13 16:50:46 -0700

    Complete requirments

--- C++ projects/dijkstra.cpp
+++ C++ projects/dijkstra.cpp
@@ -6,7 +6,7 @@ Rishabh Bhattacharya
 Jul 12th, 2023
 
 
-Program description: Create a linked list using random elements. Use bubble sort to sort the list.
+Program description: Create a random graph from given parameters, and use dijkstra's algorithm to compute average of all shortest paths.
 */
 #include<iostream>
 #include<iomanip>
@@ -14,6 +14,7 @@ Program description: Create a linked list using random elements. Use bubble sort
 #include<cstdlib>
 #include<queue>
 #include<unordered_set>
+#include<stack>
 
 
 using namespace std;
@@ -24,53 +25,59 @@ class Node {
     int prev;
 
 public:
-    Node(int data = 0, int g = 0, int prev_node = -1): val(data), cost_from_start(g), prev(prev_node) {}
+    Node(int data = 0, int g = INT_MAX, int prev_node = -1): val(data), cost_from_start(g), prev(prev_node) {}
+
+    int getVal() {return val;}
+    int getCostFromStart() {return cost_from_start;}
+    void setCostFromStart(int g) {cost_from_start = g; return;}
+    int getPrevNode() {return prev;}
+    void setPrevNode(int node) {prev = node; return;}
 };
 
 class Graph {
     const int n_nodes;
     const double density;
     const int DIST_MIN, DIST_MAX;
-    vector<vector<bool> > graph;
+    vector<vector<bool> > conn_matrix;
     vector<vector<int> > costs;
     
     double prob() {return static_cast<double>(rand()) / RAND_MAX;}
     
     void createGraph() {
         for (int i = 0; i < n_nodes; i++) {
-            for (int j = i + 1; j < n_nodes; j++) {
-                graph[i][j] = (i == j)? false : graph[j][i] = (prob() < density);
-                if (graph[i][j] == true)
+            for (int j = i; j < n_nodes; j++) {
+                conn_matrix[i][j] = conn_matrix[j][i] = (i == j)? false : (prob() < density);
+                if (i == j) costs[i][j] = costs[j][i] = 0;
+                
+                if (conn_matrix[i][j] == true)
                     costs[i][j] = costs[j][i] = rand() % (DIST_MAX - DIST_MIN + 1) + DIST_MIN;
             }
         }
     }
 
 public:
-    Graph(int n = 8, 
-          double d = 0.5, 
-          int dist_min = 1.0, 
-          int dist_max = 10.0): 
-                                n_nodes(n), 
-                                density(d), 
-                                DIST_MIN(dist_min), 
-                                DIST_MAX(dist_max), 
-                                graph(vector<vector<bool> > (n, vector<bool> (n, false))),
-                                costs(vector<vector<int> > (n, vector<int> (n, INT_MAX))) {
+    Graph(int n = 50, double d = 0.1, int dist_min = 1.0, int dist_max = 10.0, bool print_graph = false)
+        : n_nodes(n), density(d), DIST_MIN(dist_min), DIST_MAX(dist_max), 
+            conn_matrix(vector<vector<bool> > (n, vector<bool> (n, false))),
+            costs(vector<vector<int> > (n, vector<int> (n, INT_MAX))) {
         const int text_width = 20;
         
-        cout << "\nInitialized graph with\n----------------------"
+        cout << "\nInitialized graph with\n" << string(35, '-') 
             << setw(text_width) << left << "\nNodes:" <<  n_nodes
             << setw(text_width) << left  << "\nDensity:" << density
             << setw(text_width) << left  << "\nMin. distance:" << DIST_MIN
             << setw(text_width) << left  << "\nMax. distance:" << DIST_MAX
-            << "\nConn. matrix size: " << graph.size() << " x " << graph[0].size()
-            << "\nCost. matrix size: " << costs.size() << " x " << costs[0].size() << endl;
+            << "\nConn. matrix size: " << conn_matrix.size() << " x " << conn_matrix[0].size()
+            << "\nCost matrix size: " << costs.size() << " x " << costs[0].size() << endl;
         
         createGraph();
-        printMatrix(graph);
-        printMatrix(costs);
+
+        if (print_graph) {
+            printMatrix(conn_matrix);
+            printMatrix(costs);
         }
+        // cout << string(35, '-') << '\n';
+    }
     
     template <typename printable>
     void printMatrix(vector<vector<printable> >& matrix) {
@@ -82,14 +89,17 @@ public:
         cout << "\n";
     }
 
-    void printCostsFromStart() {
-        for (int i = 0; i < n_nodes; i++)
-            cout << setw(10) << right << costs[0][i] << " ";
-        
-        cout << "\n";
+    void printGraph() {
+        for (int i = 0; i < n_nodes; i++) {
+            for (int j = i + 1; j < n_nodes; j++)
+                if (costs[i][j] != INT_MAX)
+                    cout << i << " " << j << " " << costs[i][j] << "\n";
+        }
     }
 
     int getCost(int i, int j) {return costs[i][j];}
+    
+    int getNumNodes() {return n_nodes;}
 
     void setCost(int i, int j, int g) {costs[i][j] = costs[j][i] = g; return;}
 
@@ -97,7 +107,7 @@ public:
         vector<int> neighbours;
 
         for(int j = 0; j < n_nodes; j++)
-            if (graph[i][j] == true)
+            if (conn_matrix[i][j] == true)
                 neighbours.push_back(j);
         
         return neighbours;
@@ -119,7 +129,7 @@ public:
         priority_queue<Pair> temp;
 
         while(!pq.empty()) {
-            if (pq.top().first == node) {
+            if (pq.top().second == node) {
                 pq.pop();
                 pq.push(make_pair(-priority, node));
                 break;
@@ -146,18 +156,26 @@ public:
 };
 
 class ShortestPath {
-    int start;
-    int goal;
+    const int start;
+    const int goal;
     Graph G;
+    
+    vector<Node> nodes;
+    vector<vector<int> > paths;
     unordered_set<int> CLOSED;
     PriorityQueue OPEN;
 public:
-    ShortestPath(int s, int g) : start(s), goal(g) {}
+    ShortestPath(int source_node, int target_node, Graph& graph) 
+        : start(source_node), goal(target_node), G(graph), paths(vector<vector<int> >(graph.getNumNodes())){
+            for (int i = 0; i < graph.getNumNodes(); i++)
+                nodes.push_back(Node(i, (i == source_node)? 0 : INT_MAX));
+            // G.printGraph();
+        }
 
     bool dijkstra() {
         OPEN.push(start, 0);
         
-        while (!OPEN.empty() && CLOSED.find(goal) == CLOSED.end()) {
+        while (!OPEN.empty()) {
             
             int node_to_expand = OPEN.top().first;
             int cost_from_start = OPEN.top().second;
@@ -169,26 +187,71 @@ public:
                 if (CLOSED.find(neighbour) != CLOSED.end()) continue;
                 
                 int g = cost_from_start + G.getCost(node_to_expand, neighbour);
-                if (g < G.getCost(start, neighbour)) G.setCost(start, neighbour, g);             
+                if (g < nodes[neighbour].getCostFromStart()) {
+                    nodes[neighbour].setCostFromStart(g);
+                    nodes[neighbour].setPrevNode(node_to_expand);
+                };             
                 
                 if (OPEN.contains(neighbour))
-                    OPEN.changePriority(neighbour, G.getCost(start, neighbour));
+                    OPEN.changePriority(neighbour, nodes[neighbour].getCostFromStart());
                 else
-                    OPEN.push(neighbour, G.getCost(start, neighbour));
+                    OPEN.push(neighbour, nodes[neighbour].getCostFromStart());
             }
         }
+        computePaths();
+        printResults();
+        return nodes[goal].getCostFromStart() < INT_MAX;
+    }
 
-        G.printCostsFromStart();
-        return G.getCost(start, goal) < INT_MAX;
+    void computePaths() {
+        for (int i = 0; i < G.getNumNodes(); i++) {
+            int prev_node = i;
+            stack<int> path;
+            
+            while(prev_node != -1) {
+                path.push(prev_node);
+                prev_node = nodes[prev_node].getPrevNode();
+            }
+            while (!path.empty()) {
+                paths[i].push_back(path.top());
+                path.pop();
+            }
+        } 
+    }
+
+    double computeAverageDist() {
+        double avg = 0;
+        for (int i = 0; i < G.getNumNodes(); i++)
+            avg += static_cast<double>(nodes[i].getCostFromStart() - avg) / (i + 1);
+
+        return avg;
+    }
+
+    void printResults() {
+        cout << '\n' << string(20, '-') << "Computed paths" << string(20, '-') << '\n';
+
+        for (int i = 0; i < G.getNumNodes(); i++){
+            cout << "dist(" << setw(2) << left << start << " -> " << setw(2) << right << i << ") = "
+                 << setw(10) << left << nodes[i].getCostFromStart() << ": ";
+            for(int j = 0; j < paths[i].size(); j++) {
+                cout << setw(2) << right << paths[i][j];
+                if (j < paths[i].size() - 1) cout << " -> ";
+            }
+            cout << "\n";
+        }
+
+        // print if successful, and also average distance
     }
 };
 
 int main(int argc, char **argv) {
     srand(7);
     
-    ShortestPath distance (0, 3);
-    bool path_found = distance.dijkstra();
-    cout << "Path found: " << (path_found? "Yes" : "No") << endl;
+    Graph graph(50, 0.2, 1, 10, false);
+    ShortestPath shortest_path(1, 3, graph);
+    
+    bool path_found = shortest_path.dijkstra();
+    cout << "\nPath found: " << (path_found? "Yes" : "No") << endl;
     
     return 0;
 }
